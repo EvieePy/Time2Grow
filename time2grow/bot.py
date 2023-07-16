e596c77ac47186a3eba1fa88583edf5bc403eaa7
EvieePy <evieepy@gmail.com>
2023-07-17 01:46:15 +1000

    Add game logic and change Plant.update() to a coroutine. + commands.

--- time2grow/bot.py
+++ time2grow/bot.py
@@ -20,16 +20,19 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import datetime
+import logging
+import random
 from typing import Any
 
 import asqlite
-import logging
 import twitchio
 from twitchio.ext import commands, routines
 
 import core
 
 from .api import Server
+from .database import Database
 from .plant import Plant
 
 
@@ -37,18 +40,21 @@ logger: logging.Logger = logging.getLogger(__name__)
 
 
 class Bot(commands.Bot):
-
-    def __init__(self, *, server: Server, pool: asqlite.Pool) -> None:
+    def __init__(self, *, server: Server, database: Database) -> None:
         self.server = server
-        self.pool = pool
+        self.database = database
 
         self.plants: dict[str, Plant] = {}
         self._previous_dispatch: list[dict[str, Any]] = []
 
         self.game_loop.start()
 
-        config: dict[str, Any] = core.config['BOT']
-        super().__init__(token=config['token'], prefix=config['prefix'], initial_channels=config['channels'])
+        config: dict[str, Any] = core.config["BOT"]
+        super().__init__(
+            token=config["token"],
+            prefix=config["prefix"],
+            initial_channels=config["channels"],
+        )
 
     async def event_command_error(self, context: commands.Context, error: commands.TwitchCommandError) -> None:
         if isinstance(error, commands.CommandNotFound):
@@ -57,6 +63,8 @@ class Bot(commands.Bot):
         if isinstance(error, commands.CommandOnCooldown):
             return
 
+        logger.error(error)
+
     @routines.routine(minutes=1, wait_first=True)
     async def game_loop(self) -> None:
         for username, plant in self.plants.copy().items():
@@ -64,7 +72,11 @@ class Bot(commands.Bot):
                 del self.plants[username]
 
         for plant in self.plants.values():
-            plant.update()
+            await plant.update()
+
+        top: list[Plant] = list(sorted(self.plants.values(), key=lambda p: p.created))
+        for index, plant in enumerate(top, 1):
+            plant.top = index
 
         self.dispatch()
 
@@ -73,36 +85,37 @@ class Bot(commands.Bot):
 
     def dispatch(self, data: dict[str, Any] | None = None):
         if not data:
-            data = {'extra': None}
+            data = {"extra": None}
 
-        plant_data = self.plants_to_json()
-        data['plants'] = plant_data
+        plant_data: list[dict[str, Any]] = self.plants_to_json()
+        data["plants"] = plant_data
 
         self.server.dispatch(data)
         self._previous_dispatch = plant_data
 
     async def event_ready(self) -> None:
-        print(f'Logged in: {self.nick}')
+        print(f"Logged in: {self.nick}")
 
     @commands.command()
-    @commands.cooldown(1, 60, commands.Bucket.user)
-    async def seed(self, ctx: commands.Context) -> None:
-        if len(self.plants) == core.config['GAME']['available']:
-            await ctx.send('Plant house is full... Buy plant when plant house not full!')
+    @commands.cooldown(1, core.config["COOLDOWNS"]["plant"], commands.Bucket.user)
+    async def plant(self, ctx: commands.Context) -> None:
+        if len(self.plants) == core.config["GAME"]["available"]:
+            await ctx.send("Plant house is full... Buy plant when plant house not full!")
             return
 
         username: str = ctx.author.name
         if username in self.plants:
-            await ctx.send(f'@{username} you may not own more den 1 plant, limited space for plants in plant house PixelBob')
+            await ctx.send(f"{username} you may not own more then 1 plant PixelBob")
             return
 
-        self.plants[username] = Plant(username)
-        await ctx.send(f'@{username} planted a plant in the plant house! SeemsGood')
+        self.plants[username] = Plant(username, database=self.database, top=len(self.plants) + 1)
+        await ctx.send(f"{username} planted a plant in the plant house SeemsGood")
 
-        self.dispatch({'extra': {'event': 'create', 'username': username}})
+        self.dispatch({"extra": {"event": "create", "username": username}})
+        await self.database.update_stats(username, planted=1)
 
     @commands.command()
-    @commands.cooldown(1, 60, commands.Bucket.user)
+    @commands.cooldown(1, core.config["COOLDOWNS"]["water"], commands.Bucket.user)
     async def water(self, ctx: commands.Context) -> None:
         username: str = ctx.author.name
 
@@ -112,10 +125,87 @@ class Bot(commands.Bot):
 
         plant: Plant = self.plants[username]
         if plant.dead:
-            await ctx.send("Your plant is dead... Buy a new one.")
+            await ctx.send("Your plant is dead RIP. Buy a new one.")
             return
 
-        await ctx.send(f'@{username} watered their plant MyAvatar')
+        await ctx.send(f"{username} watered their plant MyAvatar")
+        await plant.update(water=True)
 
-        plant.update(water=True)
-        self.dispatch({'extra': {'event': 'water', 'username': username}})
+        self.dispatch({"extra": {"event": "water", "username": username}})
+        await self.database.update_stats(username, watered=1)
+
+    @commands.command()
+    @commands.cooldown(1, core.config["COOLDOWNS"]["thug"], commands.Bucket.user)
+    async def thug(self, ctx: commands.Context) -> None:
+        username: str = ctx.author.name
+
+        if username not in self.plants:
+            await ctx.send("You can't thug da air... Buy a plant FamilyMan")
+            return
+
+        plant: Plant = self.plants[username]
+        if plant.dead:
+            await ctx.send("Your plant is dead RIP. Buy a new one.")
+            return
+
+        await ctx.send(f"{username} thug lifed their plant GlitchCat")
+        await plant.update(glasses=True)
+
+        self.dispatch({"extra": {"event": "glasses", "username": username}})
+        await self.database.update_stats(username, thugged=1)
+
+    @commands.command()
+    @commands.cooldown(1, core.config["COOLDOWNS"]["attack"], commands.Bucket.user)
+    async def attack(self, ctx: commands.Context, *, recipient: str = "") -> None:
+        username: str = ctx.author.name
+        recipient = recipient.lower()
+
+        if recipient not in self.plants:
+            await ctx.send(f"{username} used their most special attack on the wind... It did nothing!")
+            return
+
+        if username == recipient:
+            await ctx.send(f"{username} tripped over themself. Kinda weird cause they don't have legs")
+            return
+
+        plant: Plant = self.plants[recipient]
+        if plant.dead:
+            await ctx.send(f"{username} tried to attack a ghost. But they got scared and ran away FailFish")
+            return
+
+        if plant.wilted:
+            await ctx.send(f"{username} attacked a thirsty plant. They felt bad and went to bed crying BibleThump")
+            return
+
+        attack: str = random.choice(core.config["GAME"]["attacks"])
+        if username not in self.plants:
+            await ctx.send(f"{username} used {attack} on {recipient}, it did something.")
+            await plant.update(attacked=True)
+
+        else:
+            outcome: int = random.randint(0, core.config["GAME"]["reverse_attack_chance"])
+            if outcome == 0:
+                attacker_plant: Plant = self.plants[username]
+
+                woops: str = random.choice(core.config["GAME"]["woops"])
+                await ctx.send(
+                    f"{username} used {attack} on {recipient}, but {woops}, and {recipient} stole all their water."
+                )
+
+                await plant.update(water=True)
+                await attacker_plant.update(attacked=True)
+            else:
+                await ctx.send(f"{username} used {attack} on {recipient}, it was super effective.")
+                await plant.update(attacked=True)
+
+        self.dispatch(
+            data={
+                "extra": {
+                    "event": "attacked",
+                    "attacker": username,
+                    "recipient": recipient,
+                }
+            }
+        )
+        await self.database.update_stats(recipient, victim=1)
+        await self.database.update_stats(username, sabotaged=1)
--- time2grow/plant.py
+++ time2grow/plant.py
@@ -20,37 +20,46 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import datetime
+import random
 from typing import Any
 
 import core
 
+from .database import Database
 
-TICKS: int = core.config['GAME']['ticks']
+
+TICKS: int = core.config["GAME"]["ticks"]
 if TICKS <= 0:
-    raise RuntimeError('GAME ticks must be 1 or greater.')
+    raise RuntimeError("GAME ticks must be 1 or greater.")
 
-DEATH: int = core.config['GAME']['death_cycle']
+DEATH: int = core.config["GAME"]["death_cycle"]
 if DEATH <= 1:
-    raise RuntimeError('GAME death_cycle must be 2 or greater.')
+    raise RuntimeError("GAME death_cycle must be 2 or greater.")
 
-GROWTH: int = core.config['GAME']['growth_cycles'] - 1
+GROWTH: int = core.config["GAME"]["growth_cycles"] - 1
 if GROWTH < 1:
     raise RuntimeError("GAME growth_cycles must be 1 or greater.")
 
-WATER: int = core.config['GAME']['water_cycle']
+WATER: int = core.config["GAME"]["water_cycle"]
 if WATER < 1:
     raise RuntimeError("GAME water_cycle must be 1 or greater.")
 
+GLASSES: int = core.config["GAME"]["glasses_minutes"]
+if GLASSES < 0:
+    raise RuntimeError("GAME glasses_cycle must not be negative.")
+
 
 class Plant:
+    def __init__(self, username: str, /, *, database: Database, top: int) -> None:
+        self.database = database
 
-    def __init__(self, username: str) -> None:
         self.username = username
 
         self.total: int = 0
         self._loops: int = 0
 
-        self.state: int = 0  # 0 == watered, DEATH == dead
+        self.state: int = 0  # 0 == fully watered, DEATH == dead
         self.growth: int = 0  # 0 == baby, GROWTH - 1 == Epic
 
         self.wilted: bool = False
@@ -60,41 +69,85 @@ class Plant:
         self.watering: bool = False
         self.watered_on: int = 0
 
-    def update(self, water: bool = False) -> None:
+        self.glasses: bool = False
+        self.glasses_on: int = 0
+
+        self.speech: bool = False
+        self.speech_on: int = 0
+
+        self.top = top
+        self.created: datetime.datetime = datetime.datetime.now()
+
+    async def update(self, water: bool = False, glasses: bool = False, attacked: bool = False) -> None:
         if water:
             self._loops = max(self._loops - (TICKS * WATER), 0)
+            self.state = max(self.state - WATER, 0)
             self.watered_on = self.total
             self.watering = True
+            self.wilted = False
+        elif attacked:
+            self._loops = (DEATH * TICKS) - TICKS
+            self.state = DEATH - 1
         else:
             self.total += 1
             self._loops += 1
+            await self.database.update_stats(self.username, minutes=1)
+
+        if glasses:
+            self.glasses_on = self.total
+            self.glasses = True
+
+        if self.glasses_on + GLASSES < self.total:
+            self.glasses = False
 
         if self.watered_on < self.total:
             self.watering = False
 
         self.state = int(self._loops / TICKS if self._loops % TICKS == 0 else self.state)
         if self.state == DEATH:
-            self.wilted = False
-            self.dead = True
+            await self.died()
             return
 
         self.growth = min(int(self.total / TICKS if self.total % TICKS == 0 else self.growth), GROWTH)
 
-        if self.growth == GROWTH:
+        if self.growth == GROWTH and not self.maxed:
+            await self.database.update_stats(self.username, epic=1)
             self.maxed = True
 
         self.wilted = self.state == (DEATH - 1)
+        if self.wilted:
+            await self.database.update_stats(self.username, wilted=1)
+
+        if random.randint(0, 100) > 90 and not self.speech:
+            self.speech = random.randint(1, 3)
+            self.speech_on = self.total
+
+        elif self.speech_on < self.total:
+            self.speech = False
+
+    async def died(self) -> None:
+        self.dead = True
+
+        self.wilted = False
+        self.speech = False
+        self.glasses = False
+        self.maxed = False
+
+        await self.database.update_stats(self.username, killed=1)
 
     def as_dict(self) -> dict[str, Any]:
         data = {
-            'username': self.username,
-            'state': self.state,
-            'growth': self.growth,
-            'total': self.total,
-            'wilted': self.wilted,
-            'dead': self.dead,
-            'maxed': self.maxed,
-            'watering': self.watering
+            "username": self.username,
+            "state": self.state,
+            "growth": self.growth,
+            "total": self.total,
+            "wilted": self.wilted,
+            "dead": self.dead,
+            "maxed": self.maxed,
+            "watering": self.watering,
+            "glasses": self.glasses,
+            "speech": self.speech,
+            "top": self.top,
         }
 
         return data
