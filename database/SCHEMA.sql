db7c1d44a475df06a01115c1a06fffb81b5a9fad
EvieePy <evieepy@gmail.com>
2023-07-17 01:40:35 +1000

    Update SCHEMA.sql

--- database/SCHEMA.sql
+++ database/SCHEMA.sql
@@ -5,5 +5,10 @@ CREATE TABLE IF NOT EXISTS stats (
     watered BIGINT DEFAULT 0,
     wilted BIGINT DEFAULT 0,
     killed BIGINT DEFAULT 0,
-    epic BIGINT DEFAULT 0
+    epic BIGINT DEFAULT 0,
+    sabotaged BIGINT DEFAULT 0,
+    victim BIGINT DEFAULT 0,
+    disasters BIGINT DEFAULT 0,
+    survived BIGINT DEFAULT 0,
+    thugged BIGINT DEFAULT 0
 );
